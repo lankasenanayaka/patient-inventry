ffe9ebaa67644597181b6112918a113cf217def6
Lanka Senanayaka <lanka.s@lalco.la>
2021-06-19 22:50:10 +0530

    add auth to routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,6 +40,7 @@ class HomeController extends Controller
         $widget = [
             'patients_active' => $patients_active,
             'patients_all' => $patients_all,
+            'beds_all' => $beds_all,
             'available_beds_percentage' => (int)$available_beds_percentage,
             'cured_patient_percentage' => (int)$cured_patient_percentage,
         ];
